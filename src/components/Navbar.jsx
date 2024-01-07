3d1b903331c0716e8bc31fb32eb2e979e39486da
Henry03HGBP <henry.bautista.prado@icloud.com>
2024-01-07 16:32:01 -0600

    logo incorporado

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+function Navbar(){
+  return(
+    <nav>
+     <li>
+      <img className='guitar' src='https://d1aeri3ty3izns.cloudfront.net/media/58/582287/1200/preview.jpg' />
+    </li>
+    <li>
+
+    </li>
+     
+    </nav>
+  )
+}
+
+export default Navbar
